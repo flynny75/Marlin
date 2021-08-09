72906338d490c7535fb8dece7136f6f4c79d0179
Alex <flynny75@gmail.com>
2021-08-09 12:14:13 +0100

    use full current for homing

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2581,7 +2581,7 @@
 
   #if AXIS_IS_TMC(X)
     #define X_CURRENT       800        // (mA) RMS current. Multiply by 1.414 for peak current.
-    #define X_CURRENT_HOME  350  // (mA) RMS current for sensorless homing
+    #define X_CURRENT_HOME  800        // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16        // 0..256
     #define X_RSENSE          0.11
     #define X_CHAIN_POS      -1        // -1..0: Not chained. 1: MCU MOSI connected. 2: Next in chain, ...
@@ -2599,7 +2599,7 @@
 
   #if AXIS_IS_TMC(Y)
     #define Y_CURRENT       800
-    #define Y_CURRENT_HOME  350
+    #define Y_CURRENT_HOME  800
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
     #define Y_CHAIN_POS      -1
