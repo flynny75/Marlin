dc4f71a2329198eaf6f13c92497a5eb4d387a9cf
Alex <flynny75@gmail.com>
2021-09-21 10:58:32 +0100

    Improve bed sizing, add instant fan control for better laser control

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -859,7 +859,7 @@
 #define Z_DRIVER_TYPE  TMC2209
 //#define X2_DRIVER_TYPE A4988
 //#define Y2_DRIVER_TYPE A4988
-#define Z2_DRIVER_TYPE TMC2208
+#define Z2_DRIVER_TYPE TMC2209
 //#define Z3_DRIVER_TYPE A4988
 //#define Z4_DRIVER_TYPE A4988
 //#define I_DRIVER_TYPE  A4988
@@ -1362,8 +1362,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 330
-#define Y_BED_SIZE 300
+#define X_BED_SIZE 320
+#define Y_BED_SIZE 305
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
